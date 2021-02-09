0c9ee139bae9941427727c6cdf2d8f511f33fe6d
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2021-02-08 21:51:22 -0300

    aula 05

--- src/app/pages/user-add/user-add.page.ts
+++ src/app/pages/user-add/user-add.page.ts
@@ -1,22 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
+import { Storage } from '@ionic/storage';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-user-add',
   templateUrl: './user-add.page.html',
   styleUrls: ['./user-add.page.scss'],
 })
+
 export class UserAddPage implements OnInit {
 
   user : User = new User();
 
-  constructor() { }
+  constructor(
+    private storage: Storage,
+    public alertController: AlertController
+  ) { }
 
   ngOnInit() {
   }
 
   salvar(){
-    console.log('Dados Salvos...', this.user);
+    try {
+      this.storage.set('nome', this.user.nome);
+      this.storage.set('email', this.user.email);
+      this.storage.set('senha', this.user.senha);
+      console.log('Dados Salvos...', this.user);
+      this.presentAlert();
+    } catch (error) {
+      console.error("Erro ao salvar.", error);
+    }
+    
   }
+
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alerta',
+      //subHeader: 'Subtitle',
+      message: 'Usuário cadastrado.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
 
 }
